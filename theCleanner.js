948f025ce37596a7b9331b4c2c0fef2a281654b7
OteEnded <ratnaritjumnong@gmail.com>
2024-07-15 09:20:46 +0700

    fix target [] mistake

--- theCleanner.js
+++ theCleanner.js
@@ -1,6 +1,47 @@
 const utilites = require('./theUtility.js');
+const fs = require('fs');
 
-targetPath = "Target2/"
+// Function to write JSON file
+function writeJsonFile(filePath, data) {
+
+    utilites.ensureDirectoryExistence(filePath.split('/').slice(0, -1).join('/'));
+
+    fs.readFile(filePath, 'utf8', (err, fileData) => {
+        if (err && err.code === 'ENOENT') {
+            // If file doesn't exist, create it with the new data as an array
+            fs.writeFile(filePath, JSON.stringify(data, null, 4), (err) => {
+                if (err) throw err;
+                // debug('File created and data written successfully.');
+            });
+        } else if (err) {
+            // Handle other errors
+            throw err;
+        } else {
+            // Parse the existing data
+            let jsonData;
+            try {
+                jsonData = JSON.parse(fileData);
+                if (!Array.isArray(jsonData)) {
+                    jsonData = [jsonData];
+                }
+            } catch (parseErr) {
+                console.error(debug('Error parsing JSON:', parseErr));
+                return;
+            }
+
+            // Append the new data
+            jsonData.push(data);
+
+            // Write the updated data back to the file
+            fs.writeFile(filePath, JSON.stringify(jsonData, null, 4), (err) => {
+                if (err) throw err;
+                // debug('Data appended successfully.');
+            });
+        }
+    });
+}
+
+targetPath = "Target/"
 
 fileList = utilites.listDirTree(targetPath)
 console.log(fileList);
@@ -11,11 +52,5 @@ console.log(fileList);
 for (let i = 0; i < fileList.length; i++) {
     console.log(fileList[i]);
     temp = utilites.readJsonFile(targetPath + fileList[i]);
-    for (let j = 0; j < temp.length; j++) {
-        if(Object.keys(temp[j]).includes("ก่อตั้งโดย")){
-            temp[j]["ก่อตั้งโดย"] = temp[j]["ก่อตั้งโดย"].replace(/\n/g, " ").replace(/ /g, "");
-        }
-        console.log(temp[j]);
-    }
-    utilites.writeJsonFile("./Temp/" + fileList[i], temp);
+    writeJsonFile("./Temp/" + fileList[i], temp[0]);
 }
