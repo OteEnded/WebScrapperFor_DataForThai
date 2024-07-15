1387895b47e633873e4701d2be683c3c3b33f237
OteEnded <ratnaritjumnong@gmail.com>
2024-07-15 08:56:18 +0700

    clean target's data that corrupt

--- theCleanner.js
+++ theCleanner.js
@@ -1,10 +1,16 @@
 const utilites = require('./theUtility.js');
 
-fileList = utilites.listDirTree("./Target")
+targetPath = "Target2/"
+
+fileList = utilites.listDirTree(targetPath)
+console.log(fileList);
+
+// process.exit(0);
+
 
 for (let i = 0; i < fileList.length; i++) {
     console.log(fileList[i]);
-    temp = utilites.readJsonFile("./Target/" + fileList[i]);
+    temp = utilites.readJsonFile(targetPath + fileList[i]);
     for (let j = 0; j < temp.length; j++) {
         if(Object.keys(temp[j]).includes("ก่อตั้งโดย")){
             temp[j]["ก่อตั้งโดย"] = temp[j]["ก่อตั้งโดย"].replace(/\n/g, " ").replace(/ /g, "");
